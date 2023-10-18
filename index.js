831838389592aed19f595f493083642214ae3cd1
HAARRVIN VATHARAJU <40566954+haarrvin@users.noreply.github.com>
2023-10-18 13:48:21 +0800

    Test commit to dev

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+
+const username = encodeURIComponent(process.env.mongo_db_username);
+const password = encodeURIComponent(process.env.mongo_db_password);
+const servername = process.env.mongo_db_servername;
+const MONGO_URL = `mongodb+srv://${username}:${password}${servername}`;
+
+mongoose.connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+.then(() => {
+    console.log("Connected MongoDB");
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+});
+
+const port = process.env.PORT || 8800;
+console.log("dev test");
+app.listen(port, () => {
+  console.log(`Backend server is running on port ${port}!`);
+});
+
